a8f862a6a6fd56e332313df771d37a3cce002474
Hugo Shinde <hugo.shinde@k2m.com.br>
2022-05-20 09:05:17 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './components/Menu/index'
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      
+
+    </div>
+  );
+}
+
+export default App;
