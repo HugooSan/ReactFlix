e6a92613304e4ea44e86ae9014a5dd62fb49f857
Hugo Shinde <hugo.shinde@k2m.com.br>
2022-05-26 13:46:42 -0300

    Add BannerMain

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import Menu from './components/Menu/index'
-import Carousel from './components/Carousel/index'
-import Footer from './components/Footer/index'
+import Menu from './components/Menu/index';
+import BannerMain from './components/BannerMain/index';
+import dadosIniciais from './data/dados_iniciais.json'
+import Carousel from './components/Carousel/index';
+import Footer from './components/Footer/index';
 
 
 function App() {
@@ -9,6 +11,12 @@ function App() {
     <div>
       <Menu />
       
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-End? Trabalhando na área os termos HTML, CSS e Javascript"}
+      />
+
       <Carousel/>
 
       <Footer/>
