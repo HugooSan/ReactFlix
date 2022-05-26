e61dcd6ea76e712e9fa09faac3295f4fb502dcbb
Hugo Shinde <hugo.shinde@k2m.com.br>
2022-05-26 13:13:35 -0300

    Add Carousel and Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Menu from './components/Menu/index'
+import Carousel from './components/Carousel/index'
+import Footer from './components/Footer/index'
+
 
 function App() {
   return (
     <div>
       <Menu />
       
+      <Carousel/>
 
+      <Footer/>
     </div>
   );
 }
